4fcbc02438a51188c8a2df43921a4efccb8c8e9d
morispolanco <morispolanco@gmail.com>
2023-12-25 08:41:41 -0600

    Update app.py

--- app.py
+++ app.py
@@ -3,52 +3,66 @@ from streamlit_chat import message
 from bardapi import Bard
 import json
 
-# Cargar la configuración desde un archivo JSON
+# open the config file
 with open('config.json') as config_file:
     data = json.load(config_file)
 
-# Obtener la clave de la API de Bard desde la configuración
-bard_api_key = "ABTWhQF55IO_Kq_Uc1Knk24BEfyjWjSVyMZ1yuRIL3IKNhpeyBlsjMhTkWrXllgR_gQxQVekQCaW"
+# store the bard api key in a variable
+bard_api_key = data["BARD_API_KEY"]
 
-# Inicializar la API de Bard
-bard = Bard(token=bard_api_key, timeout=30)
+# initialize the bard api
+bard = Bard(token=bard_api_key, timeout= 30)
 
-# Función para generar la respuesta del chatbot
 def generate_response(prompt):
-    history = "\n".join(f"User: {u}\nBard Response: {b}" for u, b in zip(st.session_state.user_responses, st.session_state.bard_responses))
-    response = bard.get_answer(f"Conversation History: {history}\n{prompt}")
-    return response.get("content", "Error en la respuesta de Bard")
+    # this function takes in a prompt, and then calls the bard api to get the bard response
+    history = ""
+    for user_response, bard_response in zip(st.session_state['user_responses'], st.session_state['bard_responses']):
+        history += f"User: {user_response}\n"
+        history += f"Bard Response: {bard_response}\n"
 
-# Función para manejar el clic en el botón "New Topic"
+    response = bard.get_answer(f"Conversation History: {history}\n {prompt}")   
+
+    return response["content"]
+
 def on_btn_click():
-    st.session_state.user_responses.clear()
-    st.session_state.bard_responses.clear()
+    # this function clears the session state
+    del st.session_state['user_responses']
+    del st.session_state['bard_responses']
 
-# Función para manejar el cambio en el campo de entrada de texto
 def on_input_change():
+    # this function is called when the user inputs something in the text input field
+    # it stores the user input in the session state, and then calls the generate_response function
+    # to get the bard response.
+    # it then appends the user input and bard response to the session state, and then clears the
+    # user input field.
+    
     user_input = st.session_state.user_input
     output = generate_response(user_input)
     st.session_state.user_responses.append(user_input)
     st.session_state.bard_responses.append(output)
-    st.session_state.user_input = ""
+    st.session_state["user_input"] = ""
 
-# Configuración de la interfaz de usuario
+# create the title for the page
 st.title("Bard Chatbot")
+
+# create a button to clear the session state (chat history)
 st.button("New Topic", on_click=on_btn_click)
 
-# Inicialización del estado de la sesión
-if 'user_responses' not in st.session_state:
-    st.session_state.user_responses = []
-
 if 'bard_responses' not in st.session_state:
-    st.session_state.bard_responses = []
+    st.session_state['bard_responses'] = []
 
-# Entrada de texto para la respuesta del usuario
-user_input = st.text_input("User Response:", on_change=on_input_change, key="user_input")
+if 'user_responses' not in st.session_state:
+    st.session_state['user_responses'] = []
 
-# Mostrar un spinner mientras se obtiene la respuesta de Bard
-with st.spinner("Loading..."):
-    # Mostrar el historial del chat en orden inverso
-    for i, (user_resp, bard_resp) in enumerate(reversed(list(zip(st.session_state.user_responses, st.session_state.bard_responses)))):
-        message(f"**User {i + 1}:** {user_resp}", is_user=True)
-        message(f"**Bard Response {i + 1}:** {bard_resp}")
+# create the text input field
+with st.container():
+    st.text_input("User Response:", on_change=on_input_change, key="user_input")
+
+# show a loading spinner while the bard api is getting the response
+# then show the chat history
+with st.spinner("Loading..."):   
+    if st.session_state['bard_responses']:
+
+        for i in range(len(st.session_state['bard_responses'])-1, -1, -1):
+            message(st.session_state["bard_responses"][i], key=str(i))
+            message(st.session_state['user_responses'][i], is_user=True, key=str(i) + '_user')
